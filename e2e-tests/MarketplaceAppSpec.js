9e1c4b5ba3e6924ad443b26db3fc62b592e65ee6
Andrew Hercules <andrewhercules@gmail.com>
2015-03-09 14:23:51 -0400

    Added more basic end-to-end tests for homepage features

--- e2e-tests/MarketplaceAppSpec.js
+++ e2e-tests/MarketplaceAppSpec.js
@@ -2,10 +2,19 @@ describe('Marketplace App', function() {
 
   beforeEach(function() {
     browser.get('http://127.0.0.1:3000/');
+    itemList = element.all(by.css('.item-feature'));
   });
 
   it('should have a title of Marketplace', function() {
     expect(browser.getTitle()).toEqual('Marketplace');
+  });
+
+  it('should have a search form', function() {
+    expect(element(by.id('search-input-form')).isPresent()).toBe(true);
+  });
+
+  it('should have a list of all 13 items for sale', function() {
+    expect(itemList.count()).toEqual(13);
   });
 
 });
