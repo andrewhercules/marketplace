c85ee43c331987fb00e729eab4a6a98b06d25c1a
Andrew Hercules <andrewhercules@gmail.com>
2015-03-08 20:33:44 -0400

    Created /api route to render mock database items as a JSON object

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var database = require('../inventoryDatabase');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var currentInventory = new database;
+  res.send(currentInventory.items);
 });
 
 module.exports = router;
