fe9fff62a9712a3987c879e34fcd907a5e2879f2
Andrew Hercules <andrewhercules@gmail.com>
2015-03-09 13:00:29 -0400

    Updated routes and added /mycart route

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var database = require('../inventoryDatabase');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   var currentInventory = new database;
   res.send(currentInventory.items);
