466a90a0618bf740b190b0283b3c2d00c167a1c8
Andrew Hercules <andrewhercules@gmail.com>
2015-03-12 15:51:59 -0400

    Updated controller and removed $http.get to allow app to dynamically change item inventory

--- public/controllers/applicationController.js
+++ public/controllers/applicationController.js
@@ -5,12 +5,26 @@ marketplaceApp.controller('marketplaceAppController', function($scope, $http) {
   $scope.total = 0;
   $scope.voucherError = undefined;
 
-  $http.get('/api').success(function(data) {
-    $scope.inventoryData = data;
-  });
+  $scope.inventoryData = [
+    { name: "Almond Toe Court Shoes", colour: "Patent Black", category: "Women’s Footwear", cost: 99.00, quantityInStock: 5},
+    { name: "Suede Shoes", colour: "Blue", category: "Women's Footwear", cost: 42.00, quantityInStock: 4 },
+    { name: "Leather Driver Saddle Loafers", colour: "Tan", category: "Men's Footwear", cost: 34.00, quantityInStock: 12 },
+    { name: "Flip Flops", colour: "Red", category: "Men's Footwear", cost: 19.00, quantityInStock: 6 },
+    { name: "Flip Flops", colour: "Blue", category: "Men's Footwear", cost: 19.00, quantityInStock: 0 },
+    { name: "Gold Button Cardigan", colour: "Black", category: "Women's Casualwear", cost: 167.00, quantityInStock: 6 },
+    { name: "Cotton Shorts", colour: "Medium Red", category: "Women's Casualwear", cost: 30.00, quantityInStock: 5 },
+    { name: "Fine Stripe Short Sleeve Shirt", colour: "Grey", category: "Men's Casualwear", cost: 49.99, quantityInStock: 9 },
+    { name: "Fine Stripe Short Sleeve Shirt", colour: "Green", category: "Men's Casualwear", cost: 39.99, quantityInStock: 3 },
+    { name: "Sharkskin Waistcoat", colour: "Charcoal", category: "Men's Formalwear", cost: 75.00, quantityInStock: 2 },
+    { name: "Lightweight Patch Pocket Blazer", colour: "Deer", category: "Men's Formalwear", cost: parseFloat(175.50).toFixed(2), quantityInStock: 1 },
+    { name: "￼Bird Print Dress", colour: "Black", category: "Women's Formalwear", cost: 270.00, quantityInStock: 10 },
+    { name: "￼Mid Twist Cut-Out Dress", colour: "Pink", category: "Women's Formalwear", cost: 540.00, quantityInStock: 5 }
+  ];
+
 
   $scope.addItemToCart = function(item) {
     $scope.myCart.push(item);
+    $scope.updateInventoryWhenItemAddedToCart(item);
     $scope.updateTotal('add', item);
   };
 
