dbcf26ebffb73fe993d6061318b30483b00a0d20
Andrew Hercules <andrewhercules@gmail.com>
2015-03-13 00:25:00 -0400

    Reorganized views directory and moved to inside public directory

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var api = require('./routes/api');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/public/views');
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
